beccecdb32efc540f02fbea2aa0c90bccf9c9714
Matteogatto94 <cosenza94@live.it>
2022-10-24 14:05:10 +0200

    initial commit snack2

---
+++ snack_2/main.js
@@ -0,0 +1,4 @@
+/* Snack2
+CONSEGNA: Crea 10 oggetti che rappresentano una zucchina. 
+Dividi in due array separati le zucchine che misurano meno o più di 15 cm.
+Infine stampa separatamente quanto pesano i due gruppi di zucchine. */
