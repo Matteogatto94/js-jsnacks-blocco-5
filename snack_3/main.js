da1805db33d57c102f0ab9faadf078b75955bb47
Matteogatto94 <cosenza94@live.it>
2022-10-24 14:30:32 +0200

    finish snack_3

--- snack_3/main.js
+++ snack_3/main.js
@@ -1,3 +1,16 @@
 /* Snack3
 CONSEGNA: Scrivi una funzione che accetti una stringa come argomento e la ritorni girata
 (es. Ciao -> oaiC)*/
+
+
+const word = prompt ('Type a Word');
+reverseWord(word);
+
+function reverseWord(input) {
+    let result ='';
+for (let i = input.length - 1; i >= 0; i--) {
+    const element = input[i];
+    result += element;
+}
+alert (result);
+}
